20729e8dd44d7c61b5ab02e389f652c7583a24fc
AdrianCandelas <adrian.candelas@siigroup-spain.com>
2024-05-30 11:13:43 +0200

    Antes de cambiar tablas VacationDays

--- SII_DaysOff/obj/Debug/net6.0/SII_DaysOff.AssemblyInfo.cs
+++ SII_DaysOff/obj/Debug/net6.0/SII_DaysOff.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SII_DaysOff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1162d715bd4b7e12200e1818ddf56fd7320c5627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caef61a3af4f8aba25cec9c2c81cd8b88e469a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("SII_DaysOff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SII_DaysOff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
