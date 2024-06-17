333f782c3f624e2cca7dc4ba236ebde27c0ead81
AdrianCandelas <adrian.candelas@siigroup-spain.com>
2024-06-17 16:54:51 +0200

    Cambiada fuente de letra

--- SII_DaysOff/obj/Debug/net6.0/SII_DaysOff.AssemblyInfo.cs
+++ SII_DaysOff/obj/Debug/net6.0/SII_DaysOff.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SII_DaysOff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7687868e26f53be0eb206955b5acb11450c8d1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3c1883d0f73ed196f4a8f6121660832efe85c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SII_DaysOff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SII_DaysOff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
